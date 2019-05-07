388de43036fd920c2db585bd8bfaadfa2e5294a4
oMikael <30442513+oMikael@users.noreply.github.com>
2019-05-06 20:29:50 -0500

    8)

--- src/controllers/todos.js
+++ src/controllers/todos.js
@@ -25,7 +25,7 @@ const getTodo = function(req, res) {
 const createTodo = function(req, res){
   // los ... son para copiar todo el req.body
   const todo = new Todo({
-    description: req.body,
+    description: req.body.description,
     completed: false,
     createdBy: req.user._id
   })
